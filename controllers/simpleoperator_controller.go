74fbe342377862194e8113625bd3011341234129
szikes-adam <szikes.adam@macbook>
2023-03-14 15:38:34 +0100

    Create a simple pod on cluster

--- controllers/simpleoperator_controller.go
+++ controllers/simpleoperator_controller.go
@@ -18,11 +18,15 @@ package controllers
 
 import (
 	"context"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	simpleoperatorv1alpha1 "github.com/szikes-adam/simple-kubernetes-operator/api/v1alpha1"
 )
@@ -47,9 +51,22 @@ type SimpleOperatorReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *SimpleOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	deploymentReq := &simpleoperatorv1alpha1.SimpleOperator{}
+
+	if err := r.Get(ctx, req.NamespacedName, deploymentReq); err != nil {
+		log.Error(err, "unable to fetch deployment request")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	pod := CreateDeployment(deploymentReq)
+	if err := r.Create(ctx, pod); err != nil {
+		log.Error(err, "unable to create pod")
+		return ctrl.Result{}, err
+	}
+
+	log.Info("pod is created")
 
 	return ctrl.Result{}, nil
 }
@@ -59,4 +76,30 @@ func (r *SimpleOperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&simpleoperatorv1alpha1.SimpleOperator{}).
 		Complete(r)
+}
+
+func CreateDeployment(deploymentReq *simpleoperatorv1alpha1.SimpleOperator) *corev1.Pod {
+	imageName := strings.Split(deploymentReq.Spec.Image, ":")[0]
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      imageName,
+			Namespace: deploymentReq.Namespace,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  imageName,
+					Image: deploymentReq.Spec.Image,
+					Ports: []corev1.ContainerPort{
+						{
+							Name:          "http",
+							ContainerPort: 6080,
+							Protocol:      corev1.ProtocolTCP,
+						},
+					},
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyOnFailure,
+		},
+	}
 }
